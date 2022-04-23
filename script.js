3283590e60728c5e0c1097142e143e474b2549d6
Tri Tam HOANG <tritam.hoang@gmail.com>
2022-04-23 18:34:50 +0200

    Complete computation of interest

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = principal * (1 + rate / 100 * years) - principal;
+    currentYear = new Date().getFullYear() + parseInt(years);
     
+    i = document.getElementById("result");
+    i.innerHTML = "If you deposit " + principal + ", <br />" +
+        "at an interest rate of " + rate + "%.<br />" +
+        "You will receive an amount of " + interest + ",<br />" +
+        "in the year " + currentYear + ".";
 }
         
